c0fc2e04507a56b96e3a6381dc3b4bc57dc0359e
Siva Subramanyam <ghsivasubramanyam@gmail.com>
2025-06-18 17:58:19 +0530

    updated application

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,8 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics-ktx") // Example for Firebase Analytics
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.room:room-runtime:2.5.0")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.12.0")
+ // Use the latest version
     kapt("androidx.room:room-compiler:2.5.0")
     implementation("com.google.android.material:material:1.8.0")
     // For OkHttp (networking)
